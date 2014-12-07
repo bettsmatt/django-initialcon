55bd8d41642416a960414d04901a03e198d6c9c7
Matthew Betts <matt@publons.com>
2014-12-07 18:05:41 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+INSTALL_REQUIREMENTS = ["Pillow"]
 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
